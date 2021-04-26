1e0c4cf90d80fad550244e1eb4237d6587a577f3
yuuR-Meow <rabbit3721011@outlook.com>
2021-04-26 23:02:12 +0800

    名稱改動

--- main.go
+++ main.go
@@ -138,7 +138,7 @@ func main() {
 	fmt.Println("\033[33mSmall Authentication")
 	fmt.Println("Copyright (c) 2021 yuuR-Meow aka JunWeiLiao for Information Security Class")
 	fmt.Println("\nThe source code has been uploaded to github.")
-	fmt.Println("https://github.com/yuuR-Meow/simple-file-base-muti-user-authentication\033[m")
+	fmt.Println("https://github.com/yuuR-Meow/simple-file-based-multi-user-authentication\033[m")
 	for {
 		fmt.Println()
 		if uiMode == modeGuest /*訪客模式*/ {
